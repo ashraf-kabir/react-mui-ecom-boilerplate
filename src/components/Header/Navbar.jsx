45273bc83555b732a1c6e58adde0b7fdea84c76e
ashraf-kabir <ashrafkabir95@gmail.com>
2023-03-08 15:32:20 +0600

    scroll down color change added on navbar

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -17,7 +17,7 @@ import {
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
-// const pages = ['About'];
+import ScrollToColor from './ScrollToColor';
 
 import routes from '../../routes';
 
@@ -47,64 +47,124 @@ function Navbar() {
   // };
 
   return (
-    <AppBar
-      position='fixed'
-      // style={{ background: 'rgba(0, 0, 0, 0.3)', boxShadow: 'none' }}
-      style={{ background: 'none', boxShadow: 'none' }}
-    >
-      <Container maxWidth='xl'>
-        <Toolbar disableGutters>
-          {/* add logo or icon */}
-          <Typography
-            variant='h6'
-            noWrap
-            component='a'
-            href='/'
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
-
-          {/* hamburger menu */}
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size='large'
-              aria-label='account of current user'
-              aria-controls='menu-appbar'
-              aria-haspopup='true'
-              onClick={handleOpenNavMenu}
-              color='inherit'
-            >
-              <FontAwesomeIcon icon={faBars} />
-            </IconButton>
-            <Menu
-              id='menu-appbar'
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
+    <ScrollToColor>
+      <AppBar
+        position='fixed'
+        // style={{ background: 'rgba(0, 0, 0, 0.3)', boxShadow: 'none' }}
+        style={{ background: 'none', boxShadow: 'none' }}
+      >
+        <Container maxWidth='xl'>
+          <Toolbar disableGutters>
+            {/* add logo or icon */}
+            <Typography
+              variant='h6'
+              noWrap
+              component='a'
+              href='/'
               sx={{
-                display: { xs: 'block', md: 'none' },
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              LOGO
+            </Typography>
+
+            {/* hamburger menu */}
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size='large'
+                aria-label='account of current user'
+                aria-controls='menu-appbar'
+                aria-haspopup='true'
+                onClick={handleOpenNavMenu}
+                color='inherit'
+              >
+                <FontAwesomeIcon icon={faBars} />
+              </IconButton>
+              <Menu
+                id='menu-appbar'
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' },
+                }}
+              >
+                {routes.map((route) => (
+                  <MenuItem key={route.title} onClick={handleCloseNavMenu}>
+                    <Link
+                      to={route.path}
+                      style={{
+                        color: 'inherit',
+                        textDecoration: 'none',
+                      }}
+                    >
+                      {route.title}
+                    </Link>
+                  </MenuItem>
+                ))}
+                <MenuItem onClick={handleCloseNavMenu}>
+                  <Link
+                    to='/register'
+                    style={{
+                      color: 'inherit',
+                      textDecoration: 'none',
+                    }}
+                  >
+                    Sign Up
+                  </Link>
+                </MenuItem>
+              </Menu>
+            </Box>
+
+            {/* mobile menu logo text */}
+            <Typography
+              variant='h5'
+              noWrap
+              component='a'
+              href='/'
+              sx={{
+                mr: 2,
+                display: { xs: 'flex', md: 'none' },
+                flexGrow: 1,
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              LOGO
+            </Typography>
+
+            {/* desktop nav menu */}
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: { xs: 'none', md: 'flex' },
+                justifyContent: 'flex-end',
               }}
             >
               {routes.map((route) => (
-                <MenuItem key={route.title} onClick={handleCloseNavMenu}>
+                <Button
+                  key={route.title}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                >
                   <Link
                     to={route.path}
                     style={{
@@ -114,107 +174,49 @@ function Navbar() {
                   >
                     {route.title}
                   </Link>
-                </MenuItem>
+                </Button>
               ))}
-              <MenuItem onClick={handleCloseNavMenu}>
-                <Link
-                  to='/register'
-                  style={{
-                    color: 'inherit',
-                    textDecoration: 'none',
-                  }}
-                >
-                  Sign Up
-                </Link>
-              </MenuItem>
-            </Menu>
-          </Box>
-
-          {/* mobile menu logo text */}
-          <Typography
-            variant='h5'
-            noWrap
-            component='a'
-            href='/'
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
-
-          {/* desktop nav menu */}
-          <Box
-            sx={{
-              flexGrow: 1,
-              display: { xs: 'none', md: 'flex' },
-              justifyContent: 'flex-end',
-            }}
-          >
-            {routes.map((route) => (
               <Button
-                key={route.title}
-                onClick={handleCloseNavMenu}
+                variant='contained'
                 sx={{ my: 2, color: 'white', display: 'block' }}
               >
-                <Link
-                  to={route.path}
-                  style={{
-                    color: 'inherit',
-                    textDecoration: 'none',
-                  }}
-                >
-                  {route.title}
-                </Link>
+                Sign Up
               </Button>
-            ))}
-            <Button
-              variant='contained'
-              sx={{ my: 2, color: 'white', display: 'block' }}
-            >
-              Sign Up
-            </Button>
-          </Box>
+            </Box>
 
-          <Box sx={{ flexGrow: 0, ml: 2 }}>
-            <Tooltip title='Open settings'>
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt='Remy Sharp' src='/static/images/avatar/2.jpg' />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id='menu-appbar'
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign='center'>{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+            <Box sx={{ flexGrow: 0, ml: 2 }}>
+              <Tooltip title='Open settings'>
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt='Remy Sharp' src='/static/images/avatar/2.jpg' />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: '45px' }}
+                id='menu-appbar'
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign='center'>{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ScrollToColor>
   );
 }
 export default Navbar;
