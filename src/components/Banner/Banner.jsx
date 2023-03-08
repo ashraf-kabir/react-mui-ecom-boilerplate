d07df4b62a8dc0ec0a4bdb0b79bd67980ed9c927
ashraf-kabir <ashrafkabir95@gmail.com>
2023-03-08 18:04:33 +0600

    unused code removed

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Box, Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import './Banner.css';
 
 const Banner = () => {
