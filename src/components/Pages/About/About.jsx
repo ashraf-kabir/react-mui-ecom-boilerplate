d349a06caea874672dcae92cfd021f060e9a005e
ashraf-kabir <ashrafkabir95@gmail.com>
2023-03-08 18:48:30 +0600

    basic about page added

--- src/components/Pages/About/About.jsx
+++ src/components/Pages/About/About.jsx
@@ -1,7 +1,139 @@
 import React from 'react';
+import { Container, Grid, Typography } from '@mui/material';
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <Container
+      component='main'
+      style={{ margin: 0, padding: 0 }}
+      maxWidth={false}
+    >
+      <Grid
+        container
+        sx={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: '6rem 4rem 2rem 4rem',
+        }}
+      >
+        <Grid
+          item
+          md={12}
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            padding: '0 0 2rem 0',
+          }}
+        >
+          <Typography
+            variant='h4'
+            gutterBottom
+            sx={{
+              fontWeight: 'bold',
+            }}
+          >
+            About
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          md={12}
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            padding: 0,
+          }}
+        >
+          <Typography variant='body1' gutterBottom>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam facere
+            tempore rem repudiandae minima officiis, iusto vel reiciendis eius
+            illum itaque ea. Voluptate molestias laudantium, ut quidem corrupti
+            quaerat exercitationem!
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          md={12}
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            padding: 0,
+          }}
+        >
+          <Typography variant='body1' gutterBottom>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam facere
+            tempore rem repudiandae minima officiis, iusto vel reiciendis eius
+            illum itaque ea. Voluptate molestias laudantium, ut quidem corrupti
+            quaerat exercitationem!
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          md={12}
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            padding: 0,
+          }}
+        >
+          <Typography variant='body1' gutterBottom>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam facere
+            tempore rem repudiandae minima officiis, iusto vel reiciendis eius
+            illum itaque ea. Voluptate molestias laudantium, ut quidem corrupti
+            quaerat exercitationem!
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          md={12}
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            padding: 0,
+          }}
+        >
+          <Typography variant='body1' gutterBottom>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam facere
+            tempore rem repudiandae minima officiis, iusto vel reiciendis eius
+            illum itaque ea. Voluptate molestias laudantium, ut quidem corrupti
+            quaerat exercitationem!
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          md={12}
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            padding: 0,
+          }}
+        >
+          <Typography variant='body1' gutterBottom>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam facere
+            tempore rem repudiandae minima officiis, iusto vel reiciendis eius
+            illum itaque ea. Voluptate molestias laudantium, ut quidem corrupti
+            quaerat exercitationem!
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          md={12}
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            padding: 0,
+          }}
+        >
+          <Typography variant='body1' gutterBottom>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam facere
+            tempore rem repudiandae minima officiis, iusto vel reiciendis eius
+            illum itaque ea. Voluptate molestias laudantium, ut quidem corrupti
+            quaerat exercitationem!
+          </Typography>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
 
 export default About;
