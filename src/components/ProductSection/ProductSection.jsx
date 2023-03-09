e3dc4613f3c164142021068a69d7a335708f69a1
ashraf-kabir <ashrafkabir95@gmail.com>
2023-03-09 18:01:42 +0600

    Products header added

--- src/components/ProductSection/ProductSection.jsx
+++ src/components/ProductSection/ProductSection.jsx
@@ -1,25 +1,47 @@
 import React from 'react';
-import { Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 import ProductCard from '../ProductCard/ProductCard';
 
 const ProductSection = ({ products }) => {
   return (
-    <Grid
-      item
-      xs={12}
-      sm={9}
-      sx={{
-        display: 'flex',
-        justifyContent: 'flex-start',
-        alignItems: 'flex-start',
-        flexWrap: 'wrap',
-      }}
-    >
-      {products.map((product) => (
-        <Grid item lg={3} md={4} sm={6} my={3} px={3} key={product.id}>
-          <ProductCard product={product} />
-        </Grid>
-      ))}
+    <Grid item xs={12} sm={9}>
+      <Grid item xs={12} sm={3}>
+        <Typography
+          variant='h5'
+          gutterBottom
+          sx={{
+            fontWeight: 'bold',
+          }}
+        >
+          Products
+        </Typography>
+        {/* <Typography
+          variant='body1'
+          gutterBottom
+          sx={{
+            color: '#b3b3b3',
+          }}
+        >
+          Search results for 'shoes'
+        </Typography> */}
+      </Grid>
+      <Grid
+        item
+        xs={12}
+        sm={12}
+        sx={{
+          display: 'flex',
+          justifyContent: 'flex-start',
+          alignItems: 'flex-start',
+          flexWrap: 'wrap',
+        }}
+      >
+        {products.map((product) => (
+          <Grid item lg={3} md={4} sm={6} my={3} px={3} key={product.id}>
+            <ProductCard product={product} />
+          </Grid>
+        ))}
+      </Grid>
     </Grid>
   );
 };
